b712d73365f7dc861996e613cb164ff568d49990
Dmitry Ryazantsev <dmitry.ryazantsev93@gmail.com>
2023-09-24 19:30:24 +0300

    Implemented differentiated payment logic

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -23,8 +23,13 @@ number_of_months = args.periods
 interest = args.interest / (12 * 100)  # Convert annual monthly interest rate to monthly
 
 
-def calculate_overpayment(annuity_payment, number_of_months, loan_principal):
+def calculate_annuity_overpayment(annuity_payment, number_of_months, loan_principal):
     overpayment = math.ceil(annuity_payment * number_of_months - loan_principal)
+    print(f"Overpayment = {overpayment}")
+
+
+def calculate_differentiated_overpayment(total_payments, loan_principal):
+    overpayment = math.ceil(total_payments - loan_principal)
     print(f"Overpayment = {overpayment}")
 
 
@@ -39,19 +44,30 @@ def calculate_months_to_repay(loan_principal, annuity_payment, i):
         print(f"It will take {years} year{plural_years} to repay this loan!")
     else:
         print(f"It will take {years} year{plural_years} and {months} month{plural_months} to repay this loan!")
-    calculate_overpayment(annuity_payment, number_of_months, loan_principal)
+    calculate_annuity_overpayment(annuity_payment, number_of_months, loan_principal)
 
 
 def calculate_annuity_payment(loan_principal, number_of_months, i):
     annuity_payment = math.ceil(loan_principal * (i * (1 + i) ** number_of_months) / ((1 + i) ** number_of_months - 1))
     print(f"Your annuity payment = {annuity_payment}!")
-    calculate_overpayment(annuity_payment, number_of_months, loan_principal)
+    calculate_annuity_overpayment(annuity_payment, number_of_months, loan_principal)
 
 
 def calculate_loan_principal(annuity_payment, number_of_months, i):
     loan_principal = math.floor(annuity_payment / ((i * (1 + i) ** number_of_months) / ((1 + i) ** number_of_months - 1)))
     print(f"Your loan principal = {loan_principal}!")
-    calculate_overpayment(annuity_payment, number_of_months, loan_principal)
+    calculate_annuity_overpayment(annuity_payment, number_of_months, loan_principal)
+
+
+def calculate_differentiated_payment(loan_principal, number_of_months, i):
+    total_payments = 0
+    for m in range(1, number_of_months + 1):
+        differentiated_payment = math.ceil((loan_principal / number_of_months) + (i * (loan_principal - (loan_principal * (m - 1) / number_of_months))))
+        total_payments += differentiated_payment
+        print(f"Month {m}: payment is {differentiated_payment}")
+
+    print("")
+    calculate_differentiated_overpayment(total_payments, loan_principal)
 
 
 if __name__ == '__main__':
@@ -62,7 +78,8 @@ if __name__ == '__main__':
             calculate_loan_principal(annuity_payment, number_of_months, interest)
         elif loan_type == "annuity" and number_of_months is None:
             calculate_months_to_repay(loan_principal, annuity_payment, interest)
-        else:
-            print("Incorrect parameters.")
+        elif loan_type == "diff":
+            calculate_differentiated_payment(loan_principal, number_of_months, interest)
+
     except KeyboardInterrupt:
         print("The session has been interrupted.")
